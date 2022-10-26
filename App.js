1dc355fc22fe7cc8d32d30b17ead072f228da79a
Brita Shang <brita.shang@activenetwork.com>
2022-10-26 09:42:36 +0800

    add card 1

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
 /*
 Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
@@ -9,6 +12,8 @@ export default function App() {
     return (
         <div>
             <Navbar />
+            <Hero />
+            <Card />
         </div>
     )
 }
